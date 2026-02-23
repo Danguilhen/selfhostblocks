63cd30f9aef9827bd81be20ff784115659e2eb26
sitrius <t.danguilhen@axial3d.com>
2026-02-23 23:37:04 +0000

    fix: update Prowlarr patch and configuration
    - Update nixpkgs patch to remove StateDirectory and bind mount when using custom dataDir
    - Remove mount dependencies from SelfHostBlocks Prowlarr configuration
    - Add ReadWritePaths to allow preStart script to write to data directory
    - Fixes permission issues with shb.replaceSecrets writing to /data/prowlarr

--- modules/services/arr.nix
+++ modules/services/arr.nix
@@ -626,20 +626,20 @@ in
           dataDir = cfg'.dataDir;
         };
 
-        systemd.services.prowlarr = {
-          after = [ "var-lib-private-prowlarr.mount" ];
-          requires = [ "var-lib-private-prowlarr.mount" ];
-          
-          preStart = shb.replaceSecrets {
-            userConfig =
-              cfg'.settings
-              // (lib.optionalAttrs isSSOEnabled {
-                AuthenticationRequired = "DisabledForLocalAddresses";
-                AuthenticationMethod = "External";
-              });
-            resultPath = "${cfg'.dataDir}/config.xml";
-            generator = apps.prowlarr.settingsFormat.generate;
-          };
+        systemd.services.prowlarr.serviceConfig = {
+          # Allow preStart script to write to data directory
+          ReadWritePaths = [ cfg'.dataDir ];
+        };
+
+        systemd.services.prowlarr.preStart = shb.replaceSecrets {
+          userConfig =
+            cfg'.settings
+            // (lib.optionalAttrs isSSOEnabled {
+              AuthenticationRequired = "DisabledForLocalAddresses";
+              AuthenticationMethod = "External";
+            });
+          resultPath = "${cfg'.dataDir}/config.xml";
+          generator = apps.prowlarr.settingsFormat.generate;
         };
 
         shb.nginx.vhosts = [ (vhosts { } cfg') ];
